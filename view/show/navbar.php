19abac17b16ef0cae507f28b52302f0228149bfd
Justin Bautista <justinbautista0921@gmail.com>
2024-10-10 03:52:05 -0400

    Removed the index.php to the url

--- view/show/navbar.php
+++ view/show/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar Section -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
     <div class="container">
-        <a class="navbar-brand fw-bold" href="index.php">WATCHMODE-API</a>
+        <a class="navbar-brand fw-bold" href="/watch-mode-api/">WATCHMODE-API</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
